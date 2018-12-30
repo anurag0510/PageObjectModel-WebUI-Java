ec66ffd107269cec1e5b82174373fd6affe12843
anurag0510 <anurag0510@outlook.com>
2018-12-30 11:07:32 +0530

    Moved listers to separate package.

--- src/main/java/com/listeners/TestListener.java
+++ src/main/java/com/listeners/TestListener.java
@@ -1,4 +1,4 @@
-package com.util;
+package com.listeners;
 
 import java.util.List;
 
--- src/main/java/com/listeners/WebEventListener.java
+++ src/main/java/com/listeners/WebEventListener.java
@@ -1,4 +1,4 @@
-package com.util;
+package com.listeners;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
